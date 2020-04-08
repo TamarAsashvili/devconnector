3a2c3ac35368c65310849336090a033291172f0b
Tamar <asashvili@gmail.com>
2020-04-08 19:52:00 +0200

    login and register validation I

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require('jsonwebtoken')
 const keys = require('../../config/keys')
 const passport = require('passport')
 
+
+//load input Validation
+const validateRegisterInput = require('../../validation/register');
+
 //load User model
 const User = require('../../models/User')
 
@@ -20,10 +24,18 @@ router.get('/test', (req, res) => res.json({ msg: 'users Works' }));
 //@access  Public
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    //check validation
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
+
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
-                return res.status(400).json({ email: 'Email already exists' })
+                errors.email = 'Email already exists'
+                return res.status(400).json(errors)
             } else {
                 const avatar = gravatar.url(req.body.email, {
                     s: '200', //size
