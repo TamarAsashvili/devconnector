9f60a36056b64e430055fcb6d21ffe9c57eabbbd
Tamar <asashvili@gmail.com>
2020-04-12 22:10:54 +0200

    post like & unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,6 +7,9 @@ const passport = require('passport')
 //post model
 const Post = require('../../models/Post')
 
+//profile model
+const Profile = require('../../models/Profile')
+
 //Validation
 const validatePostInput = require('../../validation/post')
 
@@ -16,6 +19,28 @@ const validatePostInput = require('../../validation/post')
 //@access  Public
 
 router.get('/test', (req, res) => res.json({ msg: 'posts Works' }));
+
+//@route   Get api/posts
+//@desc    Get post 
+//@access  Public
+
+router.get('/', (req, res) => {
+    Post.find()
+        .sort({ date: -1 })
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json({ nopostfound: 'No post found' }))
+})
+
+//@route   Get api/posts/:id
+//@desc    Get post by id
+//@access  Public
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({ nopostfound: 'No post found by ID' }))
+})
+
 
 //@route   POST api/posts
 //@desc    Create post 
@@ -40,5 +65,76 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     newPost.save().then(post => res.json(post))
 })
 
+
+//@route   DELETE api/posts/:id
+//@desc    delete post 
+//@access  Private
+
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    //check for post owner
+                    if (post.user.toString() !== req.user.id) {
+                        return res.status(401).json({ notauthorized: 'User not authorized' })
+                    }
+                    //DElete
+                    post.remove().then(() => res.json({ success: true }))
+                })
+                .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+        })
+})
+
+
+
+//@route   post api/posts/like/:id
+//@desc    like post 
+//@access  Private
+
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+                        return res.status(400).json({ alreadyliked: 'User already liked this post' })
+                    }
+                    //add user id to likes array
+                    post.likes.unshift({ user: req.user.id });
+
+
+                    post.save().then(post => res.json(post))
+                })
+                .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+        })
+})
+
+
+//@route   POST api/posts/unlike/:id
+//@desc    Unlike post 
+//@access  Private
+
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+                        return res.status(400).json({ notliked: 'You have not yet liked this post' })
+                    }
+                    //Get remove index
+                    const removeIndex = post.likes
+                        .map(item => item.user.toString())
+                        .indexOf(req.user.id)
+
+                    //splice out of array
+                    post.likes.splice(removeIndex, 1);
+                    //save
+                    post.save().then(post => res.json(post))
+                })
+                .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+        })
+})
 
 module.exports = router;
